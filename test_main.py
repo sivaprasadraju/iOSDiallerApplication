397f6547910fcd4f7ecfd927b106003cf7887d72
sraju728@gmail.com <sraju728@gmail.com>
2018-09-27 15:17:43 +0530

    Adding testcase for Add contact function

--- test_main.py
+++ test_main.py
@@ -6,3 +6,6 @@ class TestMain(TestCase):
 	
 	def TestCaseForDataDictionary(self):
         self.assertEqual(Main().Data['Trisha'], 9743274880)
+	
+	def TestCaseForAddFunction(self):
+        self.assertEqual(AddContact('siva',9542199855,Data={'prasad':9182961266, 'raju':9490738146}),3)
