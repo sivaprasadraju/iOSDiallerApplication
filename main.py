bbd06018f0ead901d66dd5147b4ee715cb629b05
sraju728@gmail.com <sraju728@gmail.com>
2018-09-27 15:15:55 +0530

    Adding Data dictionary to the main function

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 class Main:
 
     AlphabetDict = {0: [' '], 1: [''], 2: ['a', 'b', 'c'], 3: ['d', 'e', 'f'], 4:['g', 'h', 'i'], 5:['j','k','l'], 6:['m','n','o'], 7:['p','q','r','s'], 8:['t','u','v'], 9:['w','x','y','z']}
+	
+	ContactNames = ['Keanu Reeves', 'Brad Pitt', 'Trisha', 'Vijay Sethupathy']
+    ContactNumbers = [9685768789, 9687783421, 9743274880, 9949083597]
+	
+	Data = {}
+
+    Data = dict(zip(ContactNames, ContactNumbers))
