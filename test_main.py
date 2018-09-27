e17333ff890643e5f3522b37229974a0511f0b38
sraju728@gmail.com <sraju728@gmail.com>
2018-09-27 15:13:29 +0530

    Adding testcase for Data Dictionary

--- test_main.py
+++ test_main.py
@@ -3,3 +3,6 @@ from unittest import TestCase
 class TestMain(TestCase):
     def TestCaseForAlphabetDictDictionary(self):
         self.assertEqual(Main().AlphabetDict[2], ['a', 'b', 'c'])
+	
+	def TestCaseForDataDictionary(self):
+        self.assertEqual(Main().Data['Trisha'], 9743274880)
